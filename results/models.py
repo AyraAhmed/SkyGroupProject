3d7684b03e9779cea7ebe31410e3b147dabb11ab
nilamshinde252005 <nilamshinde252005@gmail.com>
2025-04-28 12:38:13 +0100

    test

--- results/models.py
+++ results/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class SeniorManagerResult(models.Model):
+    team_name = models.CharField(max_length=100)
+    department_name = models.CharField(max_length=100)
+    progress_month = models.IntegerField()  # 3, 6, or 9
+    traffic_signal = models.CharField(max_length=10)  # Red, Yellow, Green
+    count = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.team_name} - {self.department_name} - {self.progress_month} months - {self.traffic_signal}: {self.count}"
