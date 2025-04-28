0bc7bb16e56394cedd6eba615c6a1c543aa091d4
AyraAhmed <ayraahmed04@gmail.com>
2025-04-28 21:04:16 +0100

    recent updates on Ayra branch

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import ViewSummary 
 from accounts.models import UserProfile
+from django.contrib.auth.decorators import login_required
+
 
 # views for healthcheck app
 # display session selection html and summary html
 def session_selection_view(request): 
     return render(request, 'healthcheck/session_selection.html')
 
+@login_required
 def summary_view(request):
     # get the logged in user's profile
     user_profile = UserProfile.objects.get(user=request.user)
