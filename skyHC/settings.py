251159209833d0aa21088a7942c3f54c41100a21
AyraAhmed <ayraahmed04@gmail.com>
2025-04-30 18:06:03 +0100

    undid some changes

--- skyHC/settings.py
+++ skyHC/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'skyHC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # Add this line to include a global templates folder
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Add this line to include a global templates folder
         'APP_DIRS': True,  # Enable app-level templates
         'OPTIONS': {
             'context_processors': [
